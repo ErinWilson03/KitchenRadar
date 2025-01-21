3b73779092aeaf6e9e3f0cea8d40f4bdfa7205f9
ErinWilson03 <wilson-e36@ulster.ac.uk>
2025-01-21 17:08:37 +0000

    some UI changes inc. flatlist

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -3,72 +3,93 @@ import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import { Link, router } from "expo-router";
-import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Inventory from "./inventory";
 import Filters from "@/components/Filters";
+import React from "react";
+
+// FlatList: used over scrollview components when lists of dynamic items are needed
+// More memory efficient
 
 export default function Index() {
-  const handleCardPress = (route: string) => router.push(`/${route}`);
-
   return (
     <SafeAreaView className="bg-white h-full">
-      <View className="px-5">
-        <Search />
-        <View className="my-5">
-          <View className="flex flex-row items-center justify-between">
-            <Text className="text-xl font-rubik-bold text-primary-300">
-              Flagged Inventory Items
-            </Text>
-            <TouchableOpacity>
-              <Text className="text-base font-rubik-bold text-black-300">
-                See All
+      <FlatList
+        data={[1, 2, 3]}
+        renderItem={({ item }) => <ItemCard />}
+        keyExtractor={(item) => item.toString()}
+        numColumns={2}
+        contentContainerClassName="pb-32"
+        columnWrapperClassName="flex gap-5 px-5"
+        showsVerticalScrollIndicator={false}
+        // It is more efficient to put basically the whole index page in the header
+        //
+        ListHeaderComponent={
+          <View className="px-5">
+            <Search />
+            <View className="my-5">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-primary-300">
+                  Flagged Inventory Items
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base font-rubik-bold text-black-300">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
+              <Filters />
+
+              {/* Horizontal List of Items Expiring soon */}
+              <FlatList
+                data={[1, 2, 3]}
+                renderItem={({ item }) => <ItemCard />}
+                keyExtractor={(item) => item.toString()}
+                horizontal
+                bounces={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerClassName="flex gap-5 mt-5"
+              />
+            </View>
+
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-4xl font-rubik-bold text-primary-300">
+                Kitchen Hub
               </Text>
-            </TouchableOpacity>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-black-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View className="flex flex-row gap-5 mt-5">
+              <FeatureCard
+                title="Inventory"
+                description="View your full inventory"
+                imageSource={images.newYork} // Use appropriate image
+                onPress={() => router.push('/inventory')}
+              />
+
+              <FeatureCard
+                title="Shopping List"
+                description="Manage your shopping list"
+                imageSource={images.newYork} // Use appropriate image
+                onPress={() => router.push('/shopping-list')}
+              />
+            </View>
+
+            <View className="flex flex-row gap-5 mt-5">
+              <FeatureCard
+                title="Kitchen Insights"
+                description="View your use and wasteage analytics"
+                imageSource={images.newYork} // Use appropriate image
+                onPress={() => router.push('/kitchen-insights')}
+              />
+            </View>
           </View>
-          <Filters />
-          <View className="flex flex-row mt-5 gap-5">
-            <ItemCard onPress={() => handleCardPress("inventory")} />
-            <ItemCard onPress={() => handleCardPress("inventory")} />
-            <ItemCard onPress={() => handleCardPress("inventory")} />
-          </View>
-        </View>
-
-        <View className="flex flex-row items-center justify-between">
-          <Text className="text-4xl font-rubik-bold text-primary-300">
-            Kitchen Hub
-          </Text>
-          <TouchableOpacity>
-            <Text className="text-base font-rubik-bold text-black-300">
-              See All
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View className="flex flex-row gap-5 mt-5">
-          <FeatureCard
-            title="Inventory"
-            description="View your full inventory"
-            imageSource={images.newYork} // Use appropriate image
-            onPress={() => handleCardPress("inventory")}
-          />
-
-          <FeatureCard
-            title="Shopping List"
-            description="Manage your shopping list"
-            imageSource={images.newYork} // Use appropriate image
-            onPress={() => handleCardPress("shooping-list")}
-          />
-        </View>
-
-        <View className="flex flex-row gap-5 mt-5">
-          <FeatureCard
-            title="Kitchen Insights"
-            description="View your use and wasteage analytics"
-            imageSource={images.newYork} // Use appropriate image
-            onPress={() => handleCardPress("kitchen-insights")}
-          />
-        </View>
-      </View>
+        }
+      />
     </SafeAreaView>
   );
 }
